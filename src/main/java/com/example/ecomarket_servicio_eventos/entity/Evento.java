722c142535fa5e0f38df4c8e0dbd197463728bb9
el-sebas-galeano <jsebastian-galeano@javeriana.edu.co>
2024-10-04 20:50:25 -0500

    :sparkles: feat: Creación REST Eventos.

---
+++ src/main/java/com/example/ecomarket_servicio_eventos/entity/Evento.java
@@ -0,0 +1,21 @@
+package com.example.ecomarket_servicio_eventos.entity;
+
+import java.util.Date;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.Data;
+
+@Data
+@Document(collection = "eventos")
+public class Evento {
+    @Id
+    private String eventoId;
+    private String nombre;
+    private String lugar;
+    private String descripcion;
+    private int rangoPrecios;
+    private Date fechaHora;
+    private String userId;
+}
