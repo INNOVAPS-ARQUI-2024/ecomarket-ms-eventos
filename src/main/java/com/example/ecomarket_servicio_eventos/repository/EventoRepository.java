39624560e12bd82e2dd63505520d9288c89dc8e2
santiago vides <santivides01@gmail.com>
2024-10-22 14:23:35 -0500

    intento de arreglo en buscar evento por fecha

--- src/main/java/com/example/ecomarket_servicio_eventos/repository/EventoRepository.java
+++ src/main/java/com/example/ecomarket_servicio_eventos/repository/EventoRepository.java
@@ -1,5 +1,6 @@
 package com.example.ecomarket_servicio_eventos.repository;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -8,4 +9,7 @@ import com.example.ecomarket_servicio_eventos.entity.Evento;
 
 public interface EventoRepository extends MongoRepository<Evento, String>{
     List<Evento> findBySellerId(String sellerId);
+    List<Evento> findAllByUserIdsContains(String userId);
+    List<Evento> findByFechaHora(Date fechaHora);
+    List<Evento> findByFechaHoraBetween(Date startOfDay, Date endOfDay);
 }
