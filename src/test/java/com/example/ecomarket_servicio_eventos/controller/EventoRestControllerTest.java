ea12ffddcb05af20f03a390aac9c4169e8447b26
Sara Peña <sarapenagomez02@gmail.com>
2024-11-15 19:41:59 -0500

    Actualización de pruebas unitarias de EventoRestController y EventoService

--- src/test/java/com/example/ecomarket_servicio_eventos/controller/EventoRestControllerTest.java
+++ src/test/java/com/example/ecomarket_servicio_eventos/controller/EventoRestControllerTest.java
@@ -1,7 +1,9 @@
 package com.example.ecomarket_servicio_eventos.controller;
 
 
+import java.text.SimpleDateFormat;
 import java.util.Collections;
+import java.util.*;
 
 import org.junit.jupiter.api.Test;
 import static org.mockito.ArgumentMatchers.any;
@@ -32,13 +34,13 @@ public class EventoRestControllerTest {
 
 
 
-
+    //Obtener Todos los Eventos
     @Test
     public void testGivenRequestForallEventsWhenObtenerEventosEndPointIsCalledThenReturnListofEventos() throws Exception {
         //Arrange
         Mockito.when(eventoService.obtenerEventos()).thenReturn(TestUtils.mockEventos());
         //Act
-        RequestBuilder request = MockMvcRequestBuilders.get("/eventos")
+        RequestBuilder request = MockMvcRequestBuilders.get("/api/eventos")
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON_UTF8);
 
@@ -52,7 +54,7 @@ public class EventoRestControllerTest {
         //Arrange
         Mockito.when(eventoService.obtenerEventos()).thenReturn(Collections.emptyList());
         //Act
-        RequestBuilder request = MockMvcRequestBuilders.get("/eventos")
+        RequestBuilder request = MockMvcRequestBuilders.get("/api/eventos")
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON_UTF8);
 
@@ -61,13 +63,13 @@ public class EventoRestControllerTest {
                 .andExpect(MockMvcResultMatchers.status().isNoContent());
 
     }
-
+    //Obtener Evento por ID
     @Test
     public void testGivenValidEventIDWhenObtenerEventosEndpointIsCalledThenReturnListOfEventos() throws Exception {
         // Arrange
         Mockito.when(eventoService.obtenerEventoPorId(eq("123"))).thenReturn(TestUtils.mockEvento());
         // Act
-        RequestBuilder request = MockMvcRequestBuilders.get("/eventos/123")
+        RequestBuilder request = MockMvcRequestBuilders.get("/api/eventos/123")
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON_UTF8);
 
@@ -86,7 +88,7 @@ public class EventoRestControllerTest {
         Mockito.when(eventoService.guardarEvento(any())).thenReturn(TestUtils.mockEvento());
         String json = TestUtils.asJsonString(TestUtils.mockEvento());
         // Act
-        RequestBuilder request = MockMvcRequestBuilders.post("/eventos")
+        RequestBuilder request = MockMvcRequestBuilders.post("/api/eventos")
                 .content(json)
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON_UTF8);
@@ -103,7 +105,7 @@ public class EventoRestControllerTest {
 
         // Act
         RequestBuilder request = MockMvcRequestBuilders
-                .post("/eventos")  // Ruta al endpoint para crear el evento
+                .post("/api/eventos")  // Ruta al endpoint para crear el evento
                 .content(json)  // Cuerpo de la solicitud con el evento inválido
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON_UTF8);
@@ -113,7 +115,8 @@ public class EventoRestControllerTest {
                 .andExpect(MockMvcResultMatchers.status().isBadRequest());  // Verifica que se retorne 400 Bad Request
     }
 
-    //PrOMOCIONAR EVENTO
+
+    //PROMOCIONAR EVENTO
     @Test
     public void testGivenValidPromocionEventoWhenEventoisPostThenReturnisOK() throws Exception {
         //Arrange
@@ -124,7 +127,7 @@ public class EventoRestControllerTest {
         headers.add("token", "token");
 
         //Act
-        RequestBuilder request = MockMvcRequestBuilders.post("/eventos/promocion/123")
+        RequestBuilder request = MockMvcRequestBuilders.post("/api/eventos/promocion/123")
                 .content(json)
                 .headers(headers)
                 .contentType(MediaType.APPLICATION_JSON)
@@ -143,7 +146,7 @@ public class EventoRestControllerTest {
         headers.add("token", "token");
 
         //Act
-        RequestBuilder request = MockMvcRequestBuilders.post("/eventos/promocion/123")
+        RequestBuilder request = MockMvcRequestBuilders.post("/api/eventos/promocion/123")
                 .content(json)
                 .headers(headers)
                 .contentType(MediaType.APPLICATION_JSON)
@@ -164,7 +167,7 @@ public class EventoRestControllerTest {
         String json = TestUtils.asJsonString(TestUtils.mockEvento());
 
         //Act
-        RequestBuilder request = MockMvcRequestBuilders.put("/eventos/123")
+        RequestBuilder request = MockMvcRequestBuilders.put("/api/eventos/123")
                 .content(json)
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON_UTF8);
@@ -181,7 +184,7 @@ public class EventoRestControllerTest {
         String json = TestUtils.asJsonString(TestUtils.mockEvento());
 
         //Act
-        RequestBuilder request = MockMvcRequestBuilders.put("/eventos/123")
+        RequestBuilder request = MockMvcRequestBuilders.put("/api/eventos/123")
                 .content(json)
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON_UTF8);
@@ -193,37 +196,36 @@ public class EventoRestControllerTest {
 
     //ELIMINAR EVENTO
     @Test
-    public void testGivenValidEventoWhenEventoisDeletethenReturnOK() throws Exception {
-        //Arrange
-        Mockito.when(eventoService.eliminarEvento(anyString(), anyString(), anyString())).thenReturn(true);
-        String json = TestUtils.asJsonString(TestUtils.mockEvento());
+    public void testGivenValidEventoWhenEventoisDeleteThenReturnNoContent() throws Exception {
+        // Arrange
+        Mockito.when(eventoService.eliminarEvento(anyString())).thenReturn(true);
         HttpHeaders headers = new HttpHeaders();
         headers.add("correo", "test@test.com");
         headers.add("token", "token");
 
-        //Act
-        RequestBuilder request = MockMvcRequestBuilders.delete("/eventos/123")
-                .content(json)
+        // Act
+        RequestBuilder request = MockMvcRequestBuilders.delete("/api/eventos/123")
                 .headers(headers)
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON_UTF8);
 
-        //Act & Assert
+        // Act & Assert
         mockMvc.perform(request)
-                .andExpect(MockMvcResultMatchers.status().isOk());
+                .andExpect(MockMvcResultMatchers.status().isNoContent());
     }
+
 
     @Test
     public void testGivenNonValidEventoWhenEventoisDeletethenReturnNotFound() throws Exception {
         //Arrange
-        Mockito.when(eventoService.eliminarEvento(anyString(), anyString(), anyString())).thenReturn(false);
+        Mockito.when(eventoService.eliminarEvento(anyString())).thenReturn(false);
         String json = TestUtils.asJsonString(TestUtils.mockEvento());
         HttpHeaders headers = new HttpHeaders();
         headers.add("correo", "test@test.com");
         headers.add("token", "token");
 
         //Act
-        RequestBuilder request = MockMvcRequestBuilders.delete("/eventos/123")
+        RequestBuilder request = MockMvcRequestBuilders.delete("/api/eventos/123")
                 .content(json)
                 .headers(headers)
                 .contentType(MediaType.APPLICATION_JSON)
@@ -233,6 +235,179 @@ public class EventoRestControllerTest {
         mockMvc.perform(request)
                 .andExpect(MockMvcResultMatchers.status().isNotFound());
     }
+
+    // Registrar usuario en un evento
+    @Test
+    public void testRegistrarUsuario_RegistroExitoso() throws Exception {
+        // Arrange
+        String eventoId = "123";
+        String userId = "user456";
+        Map<String, String> payload = Map.of("userId", userId);
+        Mockito.when(eventoService.registrarUsuarioEnEvento(eventoId, userId)).thenReturn(true);
+
+        // Act & Assert
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/eventos/registrar/{eventoId}", eventoId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(TestUtils.asJsonString(payload))
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+
+        // Verificar que el servicio fue llamado con los parámetros correctos
+        Mockito.verify(eventoService, Mockito.times(1)).registrarUsuarioEnEvento(eventoId, userId);
+    }
+
+
+    @Test
+    public void testRegistrarUsuario_UsuarioYaRegistrado() throws Exception {
+        // Arrange
+        String eventoId = "123";
+        String userId = "user456";
+        Map<String, String> payload = Map.of("userId", userId);
+        Mockito.when(eventoService.registrarUsuarioEnEvento(eventoId, userId)).thenReturn(false);
+
+        // Act & Assert
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/eventos/registrar/{eventoId}", eventoId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(TestUtils.asJsonString(payload))
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isConflict());
+
+
+        // Verificar que el servicio fue llamado con los parámetros correctos
+        Mockito.verify(eventoService, Mockito.times(1)).registrarUsuarioEnEvento(eventoId, userId);
+    }
+
+    // Obtener eventos por fecha
+    @Test
+    public void testObtenerEventosPorFecha_FechaValidaConEventos() throws Exception {
+        // Arrange
+        String fecha = "2024-11-15";
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        Date fechaParsed = sdf.parse(fecha);
+
+        // Definir los valores de inicio y fin del día para la fecha proporcionada
+        Calendar startOfDay = Calendar.getInstance();
+        startOfDay.setTime(fechaParsed);
+        startOfDay.set(Calendar.HOUR_OF_DAY, 0);
+        startOfDay.set(Calendar.MINUTE, 0);
+        startOfDay.set(Calendar.SECOND, 0);
+        startOfDay.set(Calendar.MILLISECOND, 0);
+
+        Calendar endOfDay = Calendar.getInstance();
+        endOfDay.setTime(fechaParsed);
+        endOfDay.set(Calendar.HOUR_OF_DAY, 23);
+        endOfDay.set(Calendar.MINUTE, 59);
+        endOfDay.set(Calendar.SECOND, 59);
+        endOfDay.set(Calendar.MILLISECOND, 999);
+
+        List<Evento> eventosMock = TestUtils.mockEventos();
+        Mockito.when(eventoService.obtenerEventosPorFecha(startOfDay.getTime(), endOfDay.getTime())).thenReturn(eventosMock);
+
+        // Act & Assert
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/eventos/fecha/{fecha}", fecha)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.length()").value(eventosMock.size()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].eventoId").value(eventosMock.get(0).getEventoId()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].eventoId").value(eventosMock.get(1).getEventoId()));
+
+        // Verificar que el servicio fue llamado con los parámetros correctos
+        Mockito.verify(eventoService, Mockito.times(1)).obtenerEventosPorFecha(startOfDay.getTime(), endOfDay.getTime());
+    }
+
+    @Test
+    public void testObtenerEventosPorFecha_FechaValidaSinEventos() throws Exception {
+        // Arrange
+        String fecha = "2024-11-15";
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        Date fechaParsed = sdf.parse(fecha);
+
+        // Definir los valores de inicio y fin del día para la fecha proporcionada
+        Calendar startOfDay = Calendar.getInstance();
+        startOfDay.setTime(fechaParsed);
+        startOfDay.set(Calendar.HOUR_OF_DAY, 0);
+        startOfDay.set(Calendar.MINUTE, 0);
+        startOfDay.set(Calendar.SECOND, 0);
+        startOfDay.set(Calendar.MILLISECOND, 0);
+
+        Calendar endOfDay = Calendar.getInstance();
+        endOfDay.setTime(fechaParsed);
+        endOfDay.set(Calendar.HOUR_OF_DAY, 23);
+        endOfDay.set(Calendar.MINUTE, 59);
+        endOfDay.set(Calendar.SECOND, 59);
+        endOfDay.set(Calendar.MILLISECOND, 999);
+
+        Mockito.when(eventoService.obtenerEventosPorFecha(startOfDay.getTime(), endOfDay.getTime())).thenReturn(Collections.emptyList());
+
+        // Act & Assert
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/eventos/fecha/{fecha}", fecha)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.length()").value(0));
+
+        // Verificar que el servicio fue llamado con los parámetros correctos
+        Mockito.verify(eventoService, Mockito.times(1)).obtenerEventosPorFecha(startOfDay.getTime(), endOfDay.getTime());
+    }
+
+
+
+    @Test
+    public void testObtenerEventosPorFecha_FechaInvalida() throws Exception {
+        // Arrange
+        String fecha = "invalid-date";
+
+        // Act & Assert
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/eventos/fecha/{fecha}", fecha)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest());
+
+        // Verificar que el servicio no fue llamado debido al error de formato
+        Mockito.verify(eventoService, Mockito.never()).obtenerEventosPorFecha(Mockito.any(), Mockito.any());
+    }
+
+
+
+    // Obtener eventos por usuario
+    @Test
+    public void testObtenerEventosPorUsuario() throws Exception {
+        Mockito.when(eventoService.obtenerEventosPorUsuario(eq("user123")))
+                .thenReturn(TestUtils.mockEventos());
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/eventos/registrados/user123")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+    @Test
+    public void testObtenerEventosPorUsuarioSinEventos() throws Exception {
+        Mockito.when(eventoService.obtenerEventosPorUsuario(eq("user123")))
+                .thenReturn(Collections.emptyList());
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/eventos/registrados/user123")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+    //Eventos por Vendedor
+    @Test
+    public void testObtenerEventosPorVendedor() throws Exception {
+        // Arrange
+        String sellerId = "user123";
+        List<Evento> eventosMock = TestUtils.mockEventos();
+        Mockito.when(eventoService.obtenerEventosPorVendedor(sellerId)).thenReturn(eventosMock);
+
+        // Act & Assert
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/eventos/usuario/{sellerId}", sellerId)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.length()").value(eventosMock.size()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].eventoId").value(eventosMock.get(0).getEventoId()))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].eventoId").value(eventosMock.get(1).getEventoId()));
+
+        // Verificar que el servicio fue llamado con los parámetros correctos
+        Mockito.verify(eventoService, Mockito.times(1)).obtenerEventosPorVendedor(sellerId);
+    }
+
 
 
 }
--- src/test/java/com/example/ecomarket_servicio_eventos/service/EventoServiceTest.java
+++ src/test/java/com/example/ecomarket_servicio_eventos/service/EventoServiceTest.java
@@ -4,6 +4,7 @@ import com.example.ecomarket_servicio_eventos.entity.Evento;
 import com.example.ecomarket_servicio_eventos.repository.EventoRepository;
 import com.example.ecomarket_servicio_eventos.utils.TestUtils;
 import jakarta.ws.rs.core.Application;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -13,8 +14,8 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.test.context.ContextConfiguration;
 
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
+;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
@@ -138,7 +139,7 @@ public class EventoServiceTest {
         Mockito.doNothing().when(eventoRepository).deleteById(id);
 
         // Act
-        boolean response = eventoService.eliminarEvento(id, correo, token);
+        boolean response = eventoService.eliminarEvento(id);
 
         // Assert
         assertTrue(response);
@@ -156,7 +157,7 @@ public class EventoServiceTest {
         Mockito.when(eventoRepository.existsById(id)).thenReturn(false);
 
         // Act
-        boolean response = eventoService.eliminarEvento(id, correo, token);
+        boolean response = eventoService.eliminarEvento(id);
 
         // Assert
         assertFalse(response);
@@ -164,7 +165,109 @@ public class EventoServiceTest {
         verify(eventoRepository, never()).deleteById(anyString());
     }
 
+    //Promocionar Evento
+    @Test
+    public void testPromocionarEvento() {
+        // Arrange
+        String eventoId = "123";
+        String correo = "test@test.com";
+        String token = "valid_token";
+        Evento detalleEvento = TestUtils.mockDetallesPromocion();
 
+        // Act
+        boolean resultado = eventoService.promocionarEvento(eventoId, detalleEvento, correo, token);
 
+        // Assert
+        Assertions.assertTrue(resultado, "El método debe retornar true mientras sea un placeholder.");
+    }
+
+    //Eventos por Vendedor
+    @Test
+    public void testObtenerEventosPorVendedor() {
+        // Arrange
+        String sellerId = "user123";
+        List<Evento> eventosMock = TestUtils.mockEventos();
+        Mockito.when(eventoRepository.findBySellerId(sellerId)).thenReturn(eventosMock);
+
+        // Act
+        List<Evento> eventos = eventoService.obtenerEventosPorVendedor(sellerId);
+
+        // Assert
+        Assertions.assertNotNull(eventos, "La lista de eventos no debe ser nula");
+        Assertions.assertEquals(eventosMock.size(), eventos.size(), "Debe retornar la cantidad correcta de eventos");
+        Mockito.verify(eventoRepository, Mockito.times(1)).findBySellerId(sellerId);
+    }
+
+    //Registrar Usuario para Evento
+    @Test
+    public void testRegistrarUsuarioEnEvento() {
+        // Arrange
+        String eventoId = "123";
+        String userId = "user456";
+        Evento mockEvento = TestUtils.mockEvento();
+        mockEvento.setUserIds(new ArrayList<>()); // Simula que el usuario no está registrado
+
+        Mockito.when(eventoRepository.findById(eventoId)).thenReturn(Optional.of(mockEvento));
+
+        // Act
+        boolean registrado = eventoService.registrarUsuarioEnEvento(eventoId, userId);
+
+        // Assert
+        Assertions.assertTrue(registrado, "El usuario debería registrarse correctamente");
+        Mockito.verify(eventoRepository, Mockito.times(1)).save(mockEvento);
+        Assertions.assertTrue(mockEvento.getUserIds().contains(userId), "El ID del usuario debe ser agregado al evento");
+    }
+
+    //Eventos por Usuario
+    @Test
+    public void testObtenerEventosPorUsuario() {
+        // Arrange
+        String userId = "user456";
+        List<Evento> eventosMock = TestUtils.mockEventos();
+        Mockito.when(eventoRepository.findAllByUserIdsContains(userId)).thenReturn(eventosMock);
+
+        // Act
+        List<Evento> eventos = eventoService.obtenerEventosPorUsuario(userId);
+
+        // Assert
+        Assertions.assertNotNull(eventos, "La lista de eventos no debe ser nula");
+        Assertions.assertEquals(eventosMock.size(), eventos.size(), "Debe retornar la cantidad correcta de eventos");
+        Mockito.verify(eventoRepository, Mockito.times(1)).findAllByUserIdsContains(userId);
+    }
+
+    //Eventos por Fecha y Hora
+    @Test
+    public void testObtenerEventosPorFechaHora() {
+        // Arrange
+        Date fechaHora = new Date();
+        List<Evento> eventosMock = TestUtils.mockEventos();
+        Mockito.when(eventoRepository.findByFechaHora(fechaHora)).thenReturn(eventosMock);
+
+        // Act
+        List<Evento> eventos = eventoService.obtenerEventosPorFechaHora(fechaHora);
+
+        // Assert
+        Assertions.assertNotNull(eventos, "La lista de eventos no debe ser nula");
+        Assertions.assertEquals(eventosMock.size(), eventos.size(), "Debe retornar la cantidad correcta de eventos");
+        Mockito.verify(eventoRepository, Mockito.times(1)).findByFechaHora(fechaHora);
+    }
+
+    //Eventos por Fecha
+    @Test
+    public void testObtenerEventosPorFecha() {
+        // Arrange
+        Date startOfDay = new Date(System.currentTimeMillis() - 24 * 60 * 60 * 1000); // Ayer
+        Date endOfDay = new Date();
+        List<Evento> eventosMock = TestUtils.mockEventos();
+        Mockito.when(eventoRepository.findByFechaHoraBetween(startOfDay, endOfDay)).thenReturn(eventosMock);
+
+        // Act
+        List<Evento> eventos = eventoService.obtenerEventosPorFecha(startOfDay, endOfDay);
+
+        // Assert
+        Assertions.assertNotNull(eventos, "La lista de eventos no debe ser nula");
+        Assertions.assertEquals(eventosMock.size(), eventos.size(), "Debe retornar la cantidad correcta de eventos");
+        Mockito.verify(eventoRepository, Mockito.times(1)).findByFechaHoraBetween(startOfDay, endOfDay);
+    }
 
 }
